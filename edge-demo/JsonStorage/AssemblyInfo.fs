a680ef227302d4300433d9e180ba5e0764600e6b
Nicholas Antonov <nicholas.antonov@jet.com>
2017-06-19 17:20:34 -0400

    Add file database for skus

---
+++ edge-demo/JsonStorage/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace JsonStorage.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("JsonStorage")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("JsonStorage")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("adef1ec8-bf31-4520-9266-e3e85ef932e1")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ edge-demo/JsonStorage/Database.fs
@@ -0,0 +1,31 @@
+﻿module JsonStorage.SkuStorage
+
+open System
+open System.IO
+open Producer.Domain.Types
+open Newtonsoft.Json
+
+type Database = Map<string, ItemState>
+
+let databaseFile = "database.json"
+
+let getDatabase () : Database =
+    File.ReadAllText(databaseFile)
+    |> (fun (resText: string) -> JsonConvert.DeserializeObject<Database> resText)
+
+let writeDatabase (database: Database) =
+    database
+    |> JsonConvert.SerializeObject
+    |> (fun text -> File.WriteAllText(databaseFile, text))
+
+let getSku sku =
+    ()
+    |> getDatabase
+    |> Map.tryFind sku
+
+let updateSku (item: ItemState) =
+    ()
+    |> getDatabase
+    |> Map.add item.sku item
+    |> writeDatabase
+    
