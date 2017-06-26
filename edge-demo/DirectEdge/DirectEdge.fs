c9f319915772e72a54b1c0ed5b92aaaf94cd8439
Nicholas Antonov <nicholas.antonov@jet.com>
2017-06-26 12:27:04 -0400

    move direct edge to testing

---
+++ edge-demo/DirectEdge/DirectEdge.fs
@@ -0,0 +1,18 @@
+﻿module Producer.DirectEdge
+
+// This is exposing an "edge" contract api that should be the
+// same as calling this service using the distributed edge,
+// only without going over a protocol like http or kafka
+
+open Controllers
+open Producer.Domain.Types
+
+type ProducerInternalEdge (database: ISkuDatabase) =
+    interface IProducerApi with
+        member x.GetItem (request: GetItemRequest) : Async<GetItemResponse> = async {
+            return getItem database request
+        }
+
+        member x.UpdateQuantity (request: UpdateQuantityRequest) = async {
+            return handleUpdateQuantity database request
+        }
